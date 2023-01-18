7953036b87643bb1c06b47d64c3ac0a558f310d6
JuliansRecent <juliansrecent@gmail.com>
2023-01-18 06:05:54 -0800

    Module complete. Concatenate the variable 'name' with two string literals to output 'Hi Ada Lovelace!'.

--- src/main/java/HiAdaLovelace.java
+++ src/main/java/HiAdaLovelace.java
@@ -4,7 +4,8 @@ public class HiAdaLovelace {
     public static void main(String[] args) {
         // Assign the string "Ada Lovelace" to the variable name
         String name = "Ada Lovelace";
-
+        // Print the variable "name" to the console
+        System.out.println("Hi " + name + "!");
 
     }
 }
