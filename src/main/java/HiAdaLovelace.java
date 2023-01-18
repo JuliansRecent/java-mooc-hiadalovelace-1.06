0c0c1d51dc25a3036d9dba9cee7bc40ae3d384ae
JuliansRecent <juliansrecent@gmail.com>
2023-01-18 05:57:25 -0800

    Initial comment.

---
+++ src/main/java/HiAdaLovelace.java
@@ -0,0 +1,10 @@
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        // Assign the string "Ada Lovelace" to the variable name
+        String name = "Ada Lovelace";
+
+
+    }
+}
